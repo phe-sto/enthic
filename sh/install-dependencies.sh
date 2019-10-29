d75a8c38eec98ff321161ffb2a4fca3b08eafdbc
Christophe <christophe.brun@papit.fr>
2019-10-29 23:13:14 +0100

    Update dependency installation shell

--- sh/install-dependencies.sh
+++ sh/install-dependencies.sh
@@ -2,4 +2,11 @@
 ################################################################################
 # PROGRAM BY PAPIT SASU, INSTALL THE REQUIRED DEPENDENCIES
 ################################################################################
+# 1) INSTALL DISTANT SYNAPTIC PACKAGES
 apt-get install zip libxml2-utils mysql-server
+################################################################################
+# 2) CHANGE WORKING DIRECTORY TO PYTHON FOLDER
+cd ../py
+################################################################################
+# 3) INSTALL LOCAL PYTHON PACKAGE
+../python3_venv/bin/python3 enthic/extract_bundle.py -c ../configuration.json

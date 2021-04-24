92e9fdc756ba77e1d5a137efa18ce827c73b345b
Léonard Michelet <leonard.michelet@smile.fr>
2021-04-24 23:12:52 +0200

    fix test_extract_bundle and enhance csv separator choice Enhance regex to extract postal code and town Add Exception to stop process on error while reading town Refactor a little bit extract_bundle in order to reduce functions complexity

--- python/enthic/treat_bundle.py
+++ python/enthic/treat_bundle.py
@@ -43,14 +43,15 @@ def main():
     ############################################################################
     # RESULT FILE
     bundle_file = open(join(CONFIG['outputPath'], CONFIG['bundleFile']), "w")
+    csv_separator = CONFIG['csvSeparator']
     ############################################################################
     # READ THE INPUT FILE AND bundle_file.write THE OUTPUT
     with open(join(CONFIG['outputPath'], CONFIG['sortTmpBundleFile']), mode='r') as infile:
-        _reader = reader(infile, delimiter='\t')  # READER OF THE INPUT CSV FILE
+        _reader = reader(infile, delimiter=csv_separator)  # READER OF THE INPUT CSV FILE
         key, bundle_sum, gain, distribution = (None,) * 4
         for rows in _reader:  # ITERATE EACH LINE
             if key is not None and key < rows[0:4]:  # KEY BREAK ON BUNDLE CODE
-                bundle_file.write(";".join((key[0], key[1], key[2], key[3],
+                bundle_file.write(csv_separator.join((key[0], key[1], key[2], key[3],
                                             str(bundle_sum), "\n"))
                                   )
             if key == rows[0:4]:  # SAME KEY, SUM THE BUNDLE AMOUNT

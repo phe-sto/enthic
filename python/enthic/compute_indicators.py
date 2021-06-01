d85337a3f1e1d344ddeeae679cc0520cfd4bcc9b
Léonard Michelet <leonard.michelet@smile.fr>
2021-06-01 18:43:12 +0200

    add offset option

--- python/enthic/compute_indicators.py
+++ python/enthic/compute_indicators.py
@@ -18,21 +18,24 @@ from enthic.utils.conversion import CON_APE
 from enthic.ontology import SCORE_DESCRIPTION
 
 
-def compute_companies_statistics(host, year, limit):
+def compute_companies_statistics(host, year, limit, offset):
     """
     Compute indicators for each companies
 
         :param host: API address IP + port
     """
     indicators_count = 0
-    for i in range(1600000):
-        offset = i * limit
-        response = get("http://" + host + "/compute_all/" + str(year) + "/" + str(offset) + "/" + str(limit))
+    for i in range(offset, 2000000, limit):
+        response = get("http://" + host + "/compute_all/" + str(year) + "/" + str(i) + "/" + str(limit))
         assert response.status_code == 200, "WRONG HTTP RETURN CODE %s INSTEAD OF 200" % response.status_code
         results = loads(response.text)
         assert results is not None, "NOT RETURNING A JSON"
-        indicators_count += len(results)
-        print("For " + str(offset + limit) + " companies for year " + str(year) + ", " + str(indicators_count) + " indicators computed")
+        if len(results):
+            indicators_count += len(results)
+            print("For " + str(i + limit) + " companies for year " + str(year) + ", " + str(indicators_count) + " indicators computed")
+        else:
+            print("No more company to compute for year " + str(year) + ". " + str(indicators_count) + " indicators computed")
+            break
 
 
 def compute_ape_deciles(host, year):
@@ -65,12 +68,16 @@ def main():
                         metavar='batch size',
                         type=int,
                         help='API address IP + port')
+    parser.add_argument('offset',
+                        metavar='offset',
+                        type=int,
+                        help='API address IP + port')
 
 
     args = parser.parse_args()
     host = args.host.replace("'","")
     for year in range(2017, 2022):
-        compute_companies_statistics(host, year, args.batch_size)
+        compute_companies_statistics(host, year, args.batch_size, args.offset)
         compute_ape_deciles(host, year)
 
 if __name__ == '__main__':

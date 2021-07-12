ff08cd6a0067c853a92d0829f8d97b8078de1506
Léonard Michelet <leonard.michelet@smile.fr>
2021-07-12 14:57:48 +0200

    add code to install a server with nginx + gunicorn and enable HTTPS

--- sh/install-server.sh
+++ sh/install-server.sh
@@ -2,12 +2,23 @@
 
 ################################################################################
 # 1) INSTALL DISTANT SYNAPTIC PACKAGES
-apt-get -y install gunicorn3
+apt-get -y install gunicorn3 nginx certbot
 
 # 2) INSTALL ENTHIC SERVICE AND ENABLE IT
 cp ../server/enthic.service /etc/systemd/system/enthic.service
+mkdir -p /var/www/enthic/
 systemctl start enthic
 systemctl enable enthic
+
+# 3) CONFIGURE NGINX SERVER
+cp ../server/enthic-nginx.conf /etc/nginx/sites-available/enthic.conf
+ln -s /etc/nginx/sites-available/enthic.conf /etc/nginx/sites-enabled/
+
+systemctl enable nginx
+systemctl start nginx
+
+# 4) CONFIGURE HTTPS
+certbot --nginx
 
 # 3) INSTALL DAILY UPDATE CRON (TODO fix it)
 touch /etc/cron.daily/enthic

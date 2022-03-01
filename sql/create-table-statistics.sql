42aacf2bc7ba7a9cd8fdd4f067b91ba5fa319136
PapIT <christophe.brun@papit.fr>
2022-03-01 09:09:53 +0100

    Delete useless sql/create-table-statistics.sql

--- sql/create-table-statistics.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS annual_statistics
-(
-    siren INT NOT NULL,
-    declaration YEAR(4) NOT NULL,
-    stats_type TINYINT,
-    value FLOAT,
-    UNIQUE( `siren`, `declaration`)
-);
